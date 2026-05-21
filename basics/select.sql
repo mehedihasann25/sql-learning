ede43a3504deb34ffaffa2843346816692fb876e
Md Mehedi Hasan <mehedihasan250500@gmail.com>
2026-05-21 19:51:23 +0600

    Fix SQL queries to use 'employee' table instead of 'employees'

--- basics/select.sql
+++ basics/select.sql
@@ -3,14 +3,14 @@
 -- Course: PostgreSQL (Module 1-3)
 
 -- সব column দেখাও
-SELECT * FROM employees;
+SELECT * FROM employee;
 
--- নির্দিষ্ট column দেখাও  
-SELECT name, department, salary FROM employees;
+-- নির্দিষ্ট column দেখাও
+SELECT name, department, salary FROM employee;
 
 -- Column-কে নতুন নাম দাও (Alias)
-SELECT name AS employee_name, salary AS monthly_pay
-FROM employees;
+SELECT name AS employee_name, salary AS monthly_salary
+FROM employee;
 
 -- Note: * দিলে সব column আসে, কিন্তু large database-এ slow হয়
 --       তাই specific column name দেওয়া ভালো practice
