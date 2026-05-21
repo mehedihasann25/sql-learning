d3eed8c357cd3be5ae8421a248f30d9b958b3715
Md Mehedi Hasan <mehedihasan250500@gmail.com>
2026-05-22 00:49:04 +0600

    Refine comments in select.sql for better understanding
    Updated comments for clarity and added note on performance.

--- basics/select.sql
+++ basics/select.sql
@@ -1,8 +1,3 @@
--- Topic: SELECT Statement
--- Date: May 2026
--- Course: PostgreSQL (Module 1-3)
-
--- সব column দেখাও
 SELECT * FROM employee;
 
 -- নির্দিষ্ট column দেখাও
@@ -13,4 +8,4 @@ SELECT name AS employee_name, salary AS monthly_salary
 FROM employee;
 
 -- Note: * দিলে সব column আসে, কিন্তু large database-এ slow হয়
---       তাই specific column name দেওয়া ভালো practice
+-- তাই specific column name দেওয়া ভালো practice
